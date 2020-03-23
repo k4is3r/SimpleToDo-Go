130a280609cdeec9e02d7f6249af7dabec84d39d
EduardoImery <ejwimery@gmail.com>
2020-03-23 16:18:42 -0600

    adding a new format to print our struct

--- tareas.go
+++ tareas.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type task struct {
 	nombre      string
 	descripcion string
@@ -8,6 +10,8 @@ type task struct {
 
 func main() {
 	t := task{
-		nombre: "Completar mi curso de DeepLearning"
+		nombre:      "Completar mi curso de DeepLearning",
+		descripcion: "Completar mi cursos de DeepLearning en semana de cuarentena",
 	}
+	fmt.Printf("%+v\n", t)
 }
