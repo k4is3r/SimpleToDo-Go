62b14121ff83631b9652b0981fb1be56e2f7ba46
EduardoImery <ejwimery@gmail.com>
2020-03-23 21:17:04 -0600

    creating a list of task struct

--- tareas.go
+++ tareas.go
@@ -2,28 +2,40 @@ package main
 
 import "fmt"
 
+type taskList struct {
+	tasks []*task
+}
+
+func (t *taskList) agregarALista(tl *task) {
+	t.tasks = append(t.tasks, tl)
+}
+
 type task struct {
 	nombre      string
 	descripcion string
 	ceompletado bool
 }
 
-func (t task) marcarCompleta() {
+func (t *task) marcarCompleta() {
 	t.ceompletado = true
 }
 
-func (t task) actualizarDescripcion(descripcion string) {
+func (t *task) actualizarDescripcion(descripcion string) {
 	t.descripcion = descripcion
 }
 
-func (t task) actualizarNombre(nombre string) {
+func (t *task) actualizarNombre(nombre string) {
 	t.nombre = nombre
 }
 
 func main() {
-	t := task{
+	t := &task{
 		nombre:      "Completar mi curso de DeepLearning",
 		descripcion: "Completar mi cursos de DeepLearning en semana de cuarentena",
 	}
+	fmt.Printf("%+v\n", t)
+	t.marcarCompleta()
+	t.actualizarNombre("finalizar curso DeepLearning")
+	t.actualizarDescripcion("Completar curso lo antes posible")
 	fmt.Printf("%+v\n", t)
 }
